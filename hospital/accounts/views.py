ac34a45d401a188295f077bf2ac0f77e00f0d23d
gunasekhar20 <gunasekhar.sangaraju@gmail.com>
2025-03-13 13:06:53 +0530

    Initial Commit
    Initial Commit

--- hospital/accounts/views.py
+++ hospital/accounts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+@api_view(['POST'])
 def login_view(request):
     pass
 
+@api_view(['POST'])
+def create_patient_id(request):
+    name = request.data.get("name")
+    phone_number = request.data.get("phone_number")
+    
